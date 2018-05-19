0c9703af181d6562d8f2ce6cc18363fa518d1f1d
Tianlu Feng <t33feng@edu.uwaterloo.ca>
2018-05-19 13:21:10 -0300

    Create js script for menubar
    Change color when click on the bar

---
+++ menubar.js
@@ -0,0 +1,32 @@
+      $(document).ready(function(){
+        $("#home").mouseenter(function(){
+          $("#home").css("background-color","rgba(255, 255, 255, 0.3)");
+      });
+      $("#home").mouseleave(function(){
+        $("#home").css("background-color","rgba(255, 255, 255, 0.2)");
+  });
+});
+      $(document).ready(function(){
+        $("#intro").mouseenter(function(){
+          $("#intro").css("background-color","rgba(255, 255, 255, 0.5)");
+      });
+      $("#intro").mouseleave(function(){
+        $("#intro").css("background-color","rgba(255, 255, 255, 0.4)");
+  });
+});
+      $(document).ready(function(){
+        $("#contact").mouseenter(function(){
+          $("#contact").css("background-color","rgba(255, 255, 255, 0.7)");
+      });
+      $("#contact").mouseleave(function(){
+        $("#contact").css("background-color","rgba(255, 255, 255, 0.6)");
+  });
+});
+      $(document).ready(function(){
+        $("#resume").mouseenter(function(){
+          $("#resume").css("background-color","rgba(255, 255, 255, 0.9)");
+      });
+      $("#resume").mouseleave(function(){
+        $("#resume").css("background-color","rgba(255, 255, 255, 0.8)");
+  });
+});
